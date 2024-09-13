54e04a74cf26d672a987c71159dca4ed6b4ed695
Jackson Perry <jackperry2187@gmail.com>
2024-09-13 00:40:01 -0400

    Initial Commit

---
+++ src/main/java/jackperry2187/epitheca/EpithecaDataGenerator.java
@@ -0,0 +1,11 @@
+package jackperry2187.epitheca;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EpithecaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
